7aed70148a73fe3220b58c140d791cdc0c6ad1b6
takeike <takemasa.ikeda@konicaminolta.com>
2021-01-14 19:02:34 +0900

    implement callback

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = props => {
   const [state, setState]=useState(props);
   const {name, price} = state;
 
+  useEffect(() =>{
+    console.log('useEffect is invoked')
+  })
+  useEffect(() =>{
+    console.log('componentDidMount')},[]
+  )
+  useEffect(() =>{
+    console.log('name only')},[name]
+  )
+  
   const reset = () => {
     setState(props)
   }
 
   return (
     <>
-    <p>現在の{name}は、{price}です</p>
+    <p>現在の{name}は、{price}です </p>
     <button onClick={()=>setState({...state, price:price+1})}>+1</button>
     <button onClick={()=>setState({...state, price:price-1})}>-1</button>
     <button onClick={reset}>Reset</button>
