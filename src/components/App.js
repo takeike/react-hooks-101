54a45c0119457e1744783edc8dc675058d06025d
takeike <takemasa.ikeda@konicaminolta.com>
2021-01-18 19:09:39 +0900

    implement useContext

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import EventForm from './EventForm'
 import Events from './Events'
-
+import AppContext from '../contexts/AppContext'
 import reducer from '../reducers'
+
+console.log(AppContext)
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
 
 
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch}/>      
-      <Events state={state} dispatch={dispatch}/>      
-      
-    </div>
+    <AppContext.Provider value={'hello, world'}>
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch}/>      
+        <Events state={state} dispatch={dispatch}/>      
+      </div>
+    </AppContext.Provider>
   )
 }
 export default App;
